97a558bac6a99f3f3984decc96b2855250b0ad6e
lmarinalvz <lmarinalvz@gmail.com>
2023-04-18 12:38:49 +0200

    avanzamosMucho
    ya no me acuerdo de todos los cambios que se hicieron pero el juego es muy primitivamente jugable

--- Assets/scripts/food.cs
+++ Assets/scripts/food.cs
@@ -4,4 +4,25 @@ using UnityEngine;
 
 public class food : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D grid;
+
+    private void Start()
+    {
+        moveToRandomPosition();
+    }
+
+    public void moveToRandomPosition()
+    {
+        int randomX = (int)Random.Range(grid.bounds.min.x, grid.bounds.max.x);
+        int randomY = (int)Random.Range(grid.bounds.min.y, grid.bounds.max.y);
+        transform.position = new Vector3(randomX, randomY, 0);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("snake"))
+        {
+            moveToRandomPosition();
+        }
+    }
 }
--- Assets/scripts/snake.cs
+++ Assets/scripts/snake.cs
@@ -6,22 +6,30 @@ using UnityEngine;
 public class snake : MonoBehaviour
 {
     public Vector3 currentPosition = Vector3.right;
+    [SerializeField] private Transform segmentPrefab;
+    private List<Transform> segments;
+
+    private void Start()
+    {
+        segments = new List<Transform>();
+        segments.Add(transform);
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && currentPosition != Vector3.down)
         {
             currentPosition = Vector3.up;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) && currentPosition != Vector3.up)
         {
             currentPosition = Vector3.down;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && currentPosition != Vector3.right)
         {
             currentPosition = Vector3.left;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) && currentPosition != Vector3.left)
         {
             currentPosition = Vector3.right;
         }
@@ -29,6 +37,20 @@ public class snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
         transform.position += currentPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("food"))
+        {
+            Transform segment = Instantiate(segmentPrefab);
+            segment.position = segments[segments.Count - 1].position;
+            segments.Add(segment);
+        }
     }
 }
